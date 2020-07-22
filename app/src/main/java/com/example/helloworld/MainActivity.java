32d144d2db31125f85c2cd6cbb490b86ad366b40
Abdelrhman abduulah <abdonilsh20@fci.helwan.edu.eg>
2020-07-22 18:08:33 +0200

    handling sensor readings

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.helloworld;
 
+import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    ArrayList<Float> xAccData = new ArrayList<Float>();
+    ArrayList<Float> yAccData = new ArrayList<Float>();
+    ArrayList<Float> zAccData = new ArrayList<Float>();
 
     public boolean flag = false;
-    private TextView xAccTxt , yAccTxt , zAccTxt , xGyroTxt , yGyroTxt , zGyroTxt ;
-    private Sensor accelerometerSensor , gyroscopeSensor;
+//    float [][][] accData;
+//    float [][][] gyroData;
+
+    private static final String FILENAME = "accData.txt";
+    private TextView xAccTxt, yAccTxt, zAccTxt, xGyroTxt, yGyroTxt, zGyroTxt;
+    private Sensor accelerometerSensor, gyroscopeSensor;
     private SensorManager SM;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
         // Create out sensor maneger
-        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+        SM = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         // Accelerometer sensor
         accelerometerSensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -37,40 +57,52 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         gyroscopeSensor = SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
         // Register Accelerometer sensor listener
-        SM.registerListener(this, accelerometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         // Register Gyroscope sensor listener
-        SM.registerListener(this , gyroscopeSensor , SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener(this, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
 
         // Assign TextView
-        xAccTxt = (TextView)findViewById(R.id.xAcc);
-        yAccTxt = (TextView)findViewById(R.id.yAcc);
-        zAccTxt = (TextView)findViewById(R.id.zAcc);
-        xGyroTxt = (TextView)findViewById(R.id.xGyro);
-        yGyroTxt = (TextView)findViewById(R.id.yGyro);
-        zGyroTxt = (TextView)findViewById(R.id.zGyro);
+        xAccTxt = (TextView) findViewById(R.id.xAcc);
+        yAccTxt = (TextView) findViewById(R.id.yAcc);
+        zAccTxt = (TextView) findViewById(R.id.zAcc);
+        xGyroTxt = (TextView) findViewById(R.id.xGyro);
+        yGyroTxt = (TextView) findViewById(R.id.yGyro);
+        zGyroTxt = (TextView) findViewById(R.id.zGyro);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor sensor = sensorEvent.sensor;
 
-        while(flag){
-            // start writing into file
-        }
 
-        if(sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             xAccTxt.setText("X : " + sensorEvent.values[0]);
             yAccTxt.setText("Z : " + sensorEvent.values[1]);
             zAccTxt.setText("Z : " + sensorEvent.values[2]);
-        }else if(sensor.getType() == Sensor.TYPE_GYROSCOPE){
-            xGyroTxt.setText("X : " + sensorEvent.values[0]);
-            yGyroTxt.setText("Z : " + sensorEvent.values[1]);
-            zGyroTxt.setText("Z : " + sensorEvent.values[2]);
+
+            if (this.flag) {
+                xAccData.add(sensorEvent.values[0]);
+                yAccData.add(sensorEvent.values[1]);
+                zAccData.add(sensorEvent.values[2]);
+            }
         }
 
+
+//        if(sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+//            xAccTxt.setText("X : " + sensorEvent.values[0]);
+//            yAccTxt.setText("Z : " + sensorEvent.values[1]);
+//            zAccTxt.setText("Z : " + sensorEvent.values[2]);
+//
+//        }else if(sensor.getType() == Sensor.TYPE_GYROSCOPE){
+//            xGyroTxt.setText("X : " + sensorEvent.values[0]);
+//            yGyroTxt.setText("Z : " + sensorEvent.values[1]);
+//            zGyroTxt.setText("Z : " + sensorEvent.values[2]);
+//        }
+
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
@@ -79,12 +111,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void startRecording(View view) {
         // clean the file
-        flag = true;
-        //
+        xAccData.clear();
+        yAccData.clear();
+        zAccData.clear();
+        this.flag = true;
+
     }
 
     public void stopRecording(View view) {
-        flag = false ;
-        // send the data to backend
+        this.flag = false;
+
+
     }
+
+    // X , Y and Z values of accelerometer are stored in the three arraylists bellow :
+    //           xAccData
+    //           yAccData
+    //           zAccData
 }
